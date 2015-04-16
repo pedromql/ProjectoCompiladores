fd0935a8a92f37063a7810b02ac7e01ddb28fa54
Pedro Quitério <pedromql@gmail.com>
2015-04-16 21:28:16 +0100

    structs improved

--- structures.h
+++ structures.h
@@ -1,9 +1,9 @@
-
+#include <stdlib.h>
 
 typedef struct _node {
 	char *id;
-	Node *brother,;
-	Node *son;
+	struct _node *brother;
+	struct _node *son;
 	
 }Node;
 
@@ -11,16 +11,29 @@ typedef struct _node {
 Node *make_node(char *name,Node *brother,Node *son){
 	
 	Node *new = malloc(sizeof(Node));
+	new->id = (char*)malloc(strlen(name)*sizeof(char));
 	strcpy(new->id,name);
 	new->brother = brother;
 	new->son = son;
 	return new;
 }
 
-typedef struct _node {
-	char *id;
-	Node *brother,*son;
-}Node;
+void addBrother(Node * temp, Node * brother) {
+	while (temp->brother != NULL) {
+		temp = temp->brother;
+	}
+	temp->brother = brother;
+}
+
+void addChild(Node * temp, Node * child) {
+	temp->son = child;
+}
+
+void printAll(Node * node) {
+	if (node == NULL) return;
+	printf("bla bla bla%s\n",node->id);
+	printAll(node->son);
+	printAll(node->brother);
+}
 
 
-Node root = malloc((Node)*sizeof(int));
