bb551d5490591016344e89529e41d238eb92613e
Joao Miguel Borges Subtil <jsubtil@dei.uc.pt>
2015-05-28 01:03:04 +0100

    changes to table based search

--- META4/code_generation.c
+++ META4/code_generation.c
@@ -3,36 +3,79 @@
 #include <string.h>
 #include "thirdfase.h"
 
-void codeGeneration(arvore no,tabela_simbolos ts){
-	arvore temp;
-	if(no!=NULL){
-		//TODO
-		if(strcmp(no->tipo,"Program")==0){
-			printf("define \n");
-		}
-		
-		
-		else if(strcmp(no->tipo,"VarDecl")==0){
-			temp = no->filho;
-			while(temp!=NULL){
-				char* tipo = checkType(temp,ts);
-				printf("@%s = %s \n", );
-			}
+void generator(Node *node,Table_structure *first_table){
+	
+	Node * aux_tree=node;
+	Table_structure *aux_table=first_table;
+	Table_lines *aux_data;
+
+
+	if(aux_table != NULL){
+
+		aux_table=aux_table->next->next;//esta na program symbol table 
+		aux_data= aux_table->data;
+
+		while(aux_data->type != '_function_')
+		{
+
+			printType(aux_data);
+			aux_data=aux_data->next;
+
+
+
+
+
+
 		}
 
+		
+		
 	}
+
 
 	
 }
 
-char* checkType(arvore no,tabela_simbolos ts){
+
+
+
+
+
+void printType(Table_lines * data){
+
+	
+	char* integer = "i32";
+	char* real = "double";
+	char* boolean = "i1";
+
+	if(strcmp(data->type,"_integer_")==0){
+		printf("@%s = global %s \n",data->name,integer);
+	}
+	
+
+   	
+
+
+
+
+
+
+
+
+
+}
+
+/*char* checkType(arvore no,tabela_simbolos ts){
+	
 	tabela_simbolos tab_temp;
 	tabela_elementos elem_temp = tab_temp->elemento;
+
 	char* integer = "i32";
 	char* real = "double";
 	char* boolean = "i1";
 
 	while(elem_temp!=NULL){
+		
 		if(strcmp(elem_temp->nome,no->tipo)==0){
 			if(strcmp(elem_temp->tipo,"integer")==0){
 				return integer;
@@ -45,4 +88,18 @@ char* checkType(arvore no,tabela_simbolos ts){
 			}
 		}
 	}
-}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
