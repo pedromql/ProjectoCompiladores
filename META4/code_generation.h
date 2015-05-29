d21a760c3a1e47ee13bf2a82048305b02d4e6e07
Joao Miguel Borges Subtil <jsubtil@dei.uc.pt>
2015-05-29 22:53:29 +0100

    first stage

--- META4/code_generation.h
+++ META4/code_generation.h
@@ -3,50 +3,56 @@
 #include <string.h>
 
 
-void printType(Table_lines * data){
-
-	
-	char* integer = "i32";
-	char* real = "double";
-	char* boolean = "i1";
-
-	if(strcmp(data->type,"_integer_")==0){
-		printf("@%s = global %s \n",data->name,integer);
-	}
-	
-}
-
 void generator(Node *node,Table_structure *first_table){
 	
 	Node * aux_tree=node;
 	Table_structure *aux_table=first_table;
 	Table_lines *aux_data;
+	aux_table=aux_table->next->next;//esta na program symbol table 
 
-
-	if(aux_table != NULL){
-
-		aux_table=aux_table->next->next;//esta na program symbol table 
-		aux_data= aux_table->data;
-
-		while(aux_data->type != '_function_')
+	if(aux_tree != NULL)
+	{
+		if(strcmp(aux_tree->id,"Program")==0)
 		{
+			printf("@.strNewLine = private unnamed_addr constant [2 x i8] c\"\\0A\\00\"\n"); 
+			printf("@.strInt = private unnamed_addr constant [3 x i8] c\"%%d\\00\"\n"); 
+			printf("@.strDouble = private unnamed_addr constant [6 x i8] c\"%%.12F\\00\"\n"); 
+			printf("@.str = private unnamed_addr constant [3 x i8] c\"%%s\\00\"\n"); 
+			printf("@.strTrue = private unnamed_addr constant [5 x i8] c\"TRUE\\00\"\n"); 
+			printf("@.strFalse = private unnamed_addr constant [6 x i8] c\"FALSE\\00\"\n"); 
+			printf("\n"); 
+			printf("declare i32 @printf(i8*, ...)\n"); 
+			printf("\n");''
 
-			printType(aux_data);
-			aux_data=aux_data->next;
-
-
-
-
-
+			find_strings();
 
 		}
 
-		
-		
 	}
-
-
 	
+}
+
+void find_strings(Node * node,int level){
+
+    int i;
+    if (node == NULL) return;
+    for (i = 0; i < level; i++) {
+        
+    }
+    
+    printf("%s\n",node->id);
+    if(strcmp(node->type,"String")==0)
+    {
+
+
+
+
+
+
+    }
+
+    printAll(node->son,level+1);
+    printAll(node->brother,level);
 }
 
 
@@ -55,14 +61,10 @@ void generator(Node *node,Table_structure *first_table){
 
 
 
-   	
 
 
 
-
-
-
-
+}
 
 
 
